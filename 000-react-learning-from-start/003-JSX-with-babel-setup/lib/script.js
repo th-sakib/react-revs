85df79e5dc08a41ac5a4998e4121a42adfd0a8cd
th-sakib <mdthamidulhasansakib@gmail.com>
2025-03-01 01:35:37 +0600

    add babel setup

---
+++ 000-react-learning-from-start/003-JSX-with-babel-setup/lib/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var h2 = /*#__PURE__*/React.createElement("h1", {
+  className: "newHeading"
+}, "Hello world");
+var root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(h2);
---
+++ 000-react-learning-from-start/003-JSX-with-babel-setup/script.js
@@ -0,0 +1,4 @@
+const h2 = <h1 className={"newHeading"}>Hello world</h1>;
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(h2);

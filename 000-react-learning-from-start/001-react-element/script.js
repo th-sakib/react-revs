772e042659cef31712118f4482eb0f8f18618d8e
th-sakib <mdthamidulhasansakib@gmail.com>
2025-03-01 00:11:35 +0600

    add react element & start jsx

--- 000-react-learning-from-start/001-react-element/script.js
+++ 000-react-learning-from-start/001-react-element/script.js
@@ -12,3 +12,19 @@ const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(h2);
 
 // note: these methods in the end js codes written in the library.
+
+// we can add multiple nested elements if we want
+const container = React.createElement("div", { className: "contianer" }, [
+  React.createElement("p", { className: "para" }, "Hello I am paragraph"),
+  React.createElement("img", {
+    className: "image",
+    alt: "a test image",
+    src: "https://cloudmatetechnologies.com/wp-content/uploads/2024/06/react.js-1536x864.png",
+    height: "100px",
+  }),
+]);
+
+root.render(container);
+
+// note: if we keep complex this app it will be a huge mess and hard to maintain. So we write these codes with jsx syntax. which is parsed by "babel" into React.createElement and related methods.
+// - JSX is looks like html so its easy to write and maintain.
